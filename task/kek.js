ee89ec974e336ff3641f06006449c311b25edd26
Artyom Zabello <lanselap1488@gmail.com>
2023-05-20 16:49:43 +0200

    lesson_2_task

--- task/kek.js
+++ task/kek.js
@@ -1,3 +1,19 @@
-for (let i=0;i<5;i++){
-    console.log(i)
-}
+str1 = 'Artyom'
+logic = true
+numb1 = 22
+
+console.log(str1+logic)
+console.log(str1+numb1.toString())
+console.log(numb1.toString()+logic)
+
+console.log(str1*logic)
+console.log(str1*numb1)
+console.log(numb1*logic)
+
+console.log(str1/logic)
+console.log(str1/numb1)
+console.log(numb1/logic)
+
+console.log(String(numb1))
+console.log(Boolean(str1))
+console.log(Number(logic))
