6e3d5c31458862b40643ce7add68ea9e121021ae
Artyom Zabello <lanselap1488@gmail.com>
2023-06-03 19:14:25 +0200

    changes

--- lesson_3/task_6.js
+++ lesson_3/task_6.js
@@ -1,9 +1,8 @@
-let sample = [1,1,true,false,'A','b','A']
+let sample = [1, 1, true, false, 'A', 'b', 'A'];
 
-function myFun(arr){
-    let set = new Set(arr)
-    return Array.from(set)
-
+function myFun(arr) {
+  let set = new Set(arr);
+  return Array.from(set);
 }
 
-console.log(myFun(sample))
+console.log(myFun(sample));
