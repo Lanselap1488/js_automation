d4ad4617f92e8bc9813d10089e576aa36b37a3fc
Artyom Zabello <lanselap1488@gmail.com>
2023-06-03 19:32:07 +0200

    delete_shit

--- lesson_3/task_1.js
+++
@@ -1,6 +0,0 @@
-function arrayReverse(arr) {
-  return arr.reverse();
-}
-let list = [1, 2, 35, 7, 8, 6, 4];
-
-console.log(arrayReverse(list));
--- lesson_3/task_2.js
+++
@@ -1,7 +0,0 @@
-let list2 = [1141, 2, 35, 34112, 548, 6, 452, true];
-
-function getMax(arr) {
-  let res = arr.reduce((a, b) => Math.max(a, b));
-  return res;
-}
-console.log(getMax(list2));
